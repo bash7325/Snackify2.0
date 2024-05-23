8f45a079193d407cffc3e0120781605901e872ec
Brandon Ashby <bash7325@gmail.com>
2024-05-22 19:01:02 -0600

    fixed missing code

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -10,8 +10,11 @@ import { SnackRequest } from '../snack-request';
 export class AdminDashboardComponent implements OnInit {
   snackRequests: SnackRequest[] = [];
   orderedRequests: SnackRequest[] = [];
+  keepOnHand: SnackRequest[] = [];
   loading = true;  // Flag to indicate loading state
   error: string | null = null; // Error message
+  request: SnackRequest | undefined;
+  submitting: any;
 
   constructor(private snackRequestService: SnackRequestService) {}
 
@@ -20,6 +23,7 @@ export class AdminDashboardComponent implements OnInit {
       next: (requests: SnackRequest[]) => {
          this.snackRequests = requests.filter(req => req.ordered_flag === 0); // Filter pending requests
         this.orderedRequests = requests.filter(req => req.ordered_flag === 1); // Filter ordered requests
+        this.keepOnHand = requests.filter(req => req.keep_on_hand === 1); // Filter requests to keep on hand.
         this.loading = false;
       },
       error: (error) => {
@@ -54,6 +58,38 @@ export class AdminDashboardComponent implements OnInit {
       
     }
   }
+
+  markKeepOnHand(request: SnackRequest) {
+    if (request.keep_on_hand === 1) {
+      return; // If already marked as Keep on Hand, do nothing
+    }
+  
+    this.snackRequestService.updateKeepOnHandStatus(request.id!, !request.keep_on_hand).subscribe(
+      () => {
+        request.keep_on_hand = 1;
+        this.ngOnInit(); // this will reload all data
+      },
+      error => {
+        console.error('Error updating keep on hand status:', error);
+        // Handle the error (e.g., show an error message)
+      }
+    );
+  }
+  
+
+  markNotKeepOnHand(request: SnackRequest) {
+    this.snackRequestService.updateKeepOnHandStatus(request.id!, false).subscribe(
+      () => {
+        request.keep_on_hand = 0; 
+        this.ngOnInit(); // this will reload all data
+      },
+      error => {
+        console.error('Error updating keep on hand status:', error);
+        // Handle the error (e.g., show an error message)
+      }
+    );
+  }
+  
 
   deleteRequest(request: SnackRequest) {
     if (confirm('Are you sure you want to delete this request?')) { 
